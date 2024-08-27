9eaca5fb9cad15efe9b00e377e9d68a5c32d852f
amirSA5 <amir434soussi@gmail.com>
2024-08-27 22:12:22 +0100

    chore: enable CORS for all origins

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    // Enable CORS for all origins
+  app.enableCors({
+    origin: 'http://localhost:4200', // allow only this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // allow cookies or other credentials
+  });
   await app.listen(3000);
 }
 bootstrap();
